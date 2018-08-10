8bc6a1b6c56172f070886c24bcdef28a8252341d
Justin Lucker <jlucker@users.noreply.github.com>
2018-08-09 17:31:31 -0700

    commit

--- Polymorphism/index.js
+++ Polymorphism/index.js
@@ -17,8 +17,23 @@ function Circle () {
 extend(Circle, Shape);
 
 Circle.prototype.duplicate = function() {
-    Shape.prototype.duplicate.call(this);
     console.log('duplicate circle');
 }
 
-const c = new Circle();
+function Square() {
+
+}
+extend(Square, Shape);
+
+Square.prototype.duplicate = function() {
+    console.log('duplicate square');
+}
+
+const shapes = [
+    new Circle(),
+    new Square()
+];
+
+for (let shape of shapes) {
+    shape.duplicate();
+}
