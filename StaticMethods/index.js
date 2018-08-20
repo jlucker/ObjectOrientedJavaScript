d9cb09328a942483c138cf4699a91e02baf72660
Justin Lucker <jlucker@users.noreply.github.com>
2018-08-20 15:57:04 -0700

    commit

--- StaticMethods/index.js
+++ StaticMethods/index.js
@@ -1,20 +1,6 @@
-sayHello();
 
-// Function Declaration
-// Hoisted... raised to the top of the code
-function sayHello() {}
-
-// Function Expression
-// Not Hoisted 
-const sayGoodbye = function() {};
-
-// Class Declaration
-// Not Hoisted
-class circle {
-
-}
-
-// Class Expression
-const Square = class {
-
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
 }
